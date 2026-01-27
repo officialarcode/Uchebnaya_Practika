d809f22fd9b42735e70cc8d6d2367930e16c8f71
egor <egor>
2026-01-27 15:25:29 +0500

    api2ch

---
+++ Context/TasksContext.cs
@@ -0,0 +1,24 @@
+﻿using API_UP2.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace API_UP2.Context
+{
+    public class TasksContext
+    {
+        public DbSet<Tasks> Tasks { get; set; }
+        public TasksContext()
+        {
+            Database.EnsureCreated(); 
+            Tasks.Load(); 
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql("server=127.0.0.1;" +
+                "uid=root;" +
+                "pwd=;" +
+                "database=UP",
+                new MySqlServerVersion(new Version(8, 0, 11)));
+        }  
+    }
+}
---
+++ Controllers/TasksController.cs
@@ -0,0 +1,44 @@
+﻿using API_UP2.Context;
+using API_UP2.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API_UP2.Controllers
+{
+    [Route("api/TasksController")]
+    public class TasksController : Controller
+    {
+        [Route("List")] 
+        [HttpGet]
+        [ProducesResponseType(typeof(List<Tasks>), 200)]
+        [ProducesResponseType(500)]
+        public ActionResult List()
+        {
+            try
+            {
+                IEnumerable<Tasks> Tasks = new TasksContext().Tasks;
+                return Json(Tasks); 
+            }
+            catch (Exception exp)
+            {
+                return StatusCode(500);
+            }
+        }
+
+        [Route("Item")] 
+        [HttpGet]
+        [ProducesResponseType(typeof(Tasks), 200)]
+        [ProducesResponseType(500)]
+        public ActionResult Item(int Id)
+        {
+            try
+            {
+                Tasks Task = new TasksContext().Tasks.Where(x => x.Id == Id).First();
+                return Json(Task); 
+            }
+            catch (Exception exp)
+            {
+                return StatusCode(500);
+            }
+        }
+    }
+}
