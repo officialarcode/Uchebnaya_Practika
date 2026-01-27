eae4cd84e0918a8836c6a3ff6750d2a172431df5
egor <egor>
2026-01-27 15:12:40 +0500

    api1

--- StartUp.cs
+++ StartUp.cs
@@ -1,6 +1,38 @@
-﻿namespace API_UP2
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+using System.IO;
+
+
+namespace API_UP2
 {
     public class StartUp
     {
+        public void ConfigureServices(IServiceCollection services){
+            services.AddMvc(option => option.EnableEndpointRouting = false);
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "API_UP",
+                    Description = "API"
+                });
+                var filePath = Path.Combine(System.AppContext.BaseDirectory, "ASP_GET.xml");
+                c.IncludeXmlComments(filePath);
+            });
+        }
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseStatusCodePages();
+            app.UseMvcWithDefaultRoute();
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Запросы GET");
+            });
+        }
     }
 }
