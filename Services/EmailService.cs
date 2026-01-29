cf41367e668395d214f15d4671b17c74626c0969
egor <egor>
2026-01-29 15:21:25 +0500

    доделать отправку кода на email

---
+++ Services/EmailService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace API_UP2.Services 
+
+{
+	public class EmailService
+	{
+		private readonly IConfiguration _configuration;
+		public EmailService(IConfiguration configuration)
+		{
+            _configuration = configuration;	
+        }
+		public async Task<bool> SendPasswordResetEmailAsync(string email, string resetCode)
+		{
+			
+		}
+}
