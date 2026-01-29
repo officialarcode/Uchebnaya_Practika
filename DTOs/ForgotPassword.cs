c7c8b00a0fbc311934bfcf58fd735062f43bad1f
egor <egor>
2026-01-29 10:39:46 +0500

    DTO на восстановление пароля

---
+++ DTOs/ForgotPassword.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace API_UP2.DTOs
+{
+	public class ForgotPassword
+	{
+		public string Email { get; set; }
+
+	}
+	public class ResetPassword
+	{
+		public string Email { get; set; }
+        public string Code { get; set; }
+        public string NewPassword { get; set; }
+        public string ConfirmPassword { get; set; }
+    }
+}
--- Models/User.cs
+++ Models/User.cs
@@ -4,4 +4,7 @@
     public string Username { get; set; }
     public string PasswordHash { get; set; }
     public string RoleId { get; set; }
+    public string Email { get; set; }
+    public string ResetPassword { get;set;}
+
 }
